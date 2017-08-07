ab816ed93a18c8b45cf6c76f7439ba995f9cf725
Em An <study.eman@gmail.com>
2017-08-07 11:24:07 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,108 +2,104 @@ $(window).on('load', function() {
   $('.load').delay(3000).fadeOut(1000).fadeOut(1000);
 });
 $(function() {
-    // All Variable
-    var slider = $(".slider"),
-        sliderUl = slider.find("ul"),
-        sliderUlLi = sliderUl.find("li"),
-        sliderOl = slider.find("ol"),
-        sliderOlLi = sliderOl.find("li"),
-        controlFa = $(".control .fa"),
-        sliderTime = 1000,
+    // All Variable =======================================================================
+    var body        = $('body'),
+        slider      = $(".slider"),
+        sliderUl    = slider.find("ul"),
+        sliderUlLi  = sliderUl.find("li"),
+        sliderOl    = slider.find("ol"),
+        sliderOlLi  = sliderOl.find("li"),
+        controlFa   = $(".control .fa"),
+        sliderTime  = 900,
+        sliderWait  = 4000,
+        clickHere   = "yes click",
         autoPlay;
 
+    // All Functions =====================================================================
     sliderUl.append("<li>" + sliderUlLi.first().html() + "</li>");
     sliderUl.prepend("<li>" + sliderUlLi.last().html() + "</li>");
-
-    // All Functions
+    
     function runSlider() {
-        if (sliderOlLi.hasClass("active")) {
-            sliderUl.animate(
-                {
-                    marginLeft:
-                        -sliderUlLi.width() * ($(".slider .active").index() + 1)
-                },
-                sliderTime
-            );
+        if (clickHere === "yes click") {
+            clickHere = "no click";
+            sliderUl.animate({
+                marginLeft: -sliderUlLi.width() * ($(".slider .active").index() + 1)
+            }, sliderTime, function () { clickHere = "yes click"; });
         }
     }
     function addActive(param) {
-        param.addClass("active").siblings("li").removeClass("active");
+        if (clickHere === "yes click") {
+            param.addClass("active").siblings("li").removeClass("active");
+        }
     }
 
-    // Click Point
+    // Click Point =======================================================================
     sliderOlLi.on("click", function() {
         addActive($(this));
         runSlider();
     });
 
-    // Click Arrow
-    controlFa.on("click", function() {
-        if ($(this).hasClass("fa-angle-left")) {
-            if ($(".slider .active").is(":first-of-type")) {
-                addActive(sliderOlLi.last());
-                sliderUl.animate(
-                    {
-                        marginLeft: "+=" + sliderUlLi.first().width()
-                    },
-                    sliderTime,
-                    function() {
-                        sliderUl.css(
-                            "margin-left",
-                            -sliderUlLi.width() *
-                                ($(".slider .active").index() + 1)
-                        );
-                    }
-                );
-            } else {
-                addActive($(".slider .active").prev("li"));
-                runSlider();
+    // Click Arrow Left
+    controlFa.first().on("click", function() {
+        if ($(".slider .active").is(":first-of-type")) {
+            addActive(sliderOlLi.last());
+            if (clickHere === "yes click") {
+                clickHere = "no click";
+                sliderUl.animate({
+                    marginLeft: "+=" + sliderUlLi.first().width()
+                }, sliderTime, function() {
+                    sliderUl.css( "margin-left", -sliderUlLi.width() * ($(".slider .active").index() + 1));
+                    clickHere = "yes click";
+                });
             }
-        }
-        if ($(this).hasClass("fa-angle-right")) {
-            if ($(".slider .active").is(":last-of-type")) {
-                addActive(sliderOlLi.first());
-                sliderUl.animate(
-                    {
-                        marginLeft: "-=" + sliderUlLi.first().width()
-                    },
-                    sliderTime,
-                    function() {
-                        sliderUl.css(
-                            "margin-left",
-                            -sliderUlLi.width() *
-                                ($(".slider .active").index() + 1)
-                        );
-                    }
-                );
-            } else {
-                addActive($(".slider .active").next("li"));
-                runSlider();
-            }
+        } else {
+            addActive($(".slider .active").prev("li"));
+            runSlider();
         }
     });
 
-    // Keyboard
-    $("body").keydown(function(e) {
-  if(e.keyCode == 37) { // left
-    controlFa.first().click();
-  }
-  else if(e.keyCode == 39) { // right
-    controlFa.last().click();
-  }
-});
-    
-    // Start Set
+    // Click Arrow Right
+    controlFa.last().on("click", function() {
+        if ($(".slider .active").is(":last-of-type")) {
+            addActive(sliderOlLi.first());
+            if (clickHere === "yes click") {
+                clickHere = "no click";
+                sliderUl.animate({
+                    marginLeft: "-=" + sliderUlLi.first().width()
+                }, sliderTime, function() { 
+                    sliderUl.css("margin-left", -sliderUlLi.width() * ($(".slider .active").index() + 1));
+                    clickHere = "yes click";
+                });
+            }
+        } else {
+            addActive($(".slider .active").next("li"));
+            runSlider();
+        }
+    });
+                
+    // Start Set =======================================================================
     sliderOlLi.first().click();
-
-    $(".view").each(function() {
-        $(this).css("background-image", "url(" + $(this).data("image") + ")");
-    });
     
-    // Transition
-    $('.project .screen, .project .tablet, .project .mobile').hover(function () {
-        $(this).find('.view').css('transition', 'all ' + $(this).find('.view').data('second') + 's ease-in-out');
-    }, function () {
-        $(this).find('.view').css('transition', 'all 1s ease-in-out');
+    // Keyboard ========================================================================
+    $("body").keydown(function(e) {
+        if (e.keyCode == 37) { // left
+            controlFa.first().click();
+        } else if (e.keyCode == 39) { // right
+            controlFa.last().click();
+        }
     });
+
+    // Auto Run Slider ==============================================================
+    function autoRunSlider() {
+        if (body.css('direction') === 'ltr') {
+            autoPlay = setInterval(function() { controlFa.last().click(); }, sliderWait);
+        } else if (body.css('direction') === 'rtl') {
+            autoPlay = setInterval(function() { controlFa.first().click(); }, sliderWait);
+        }
+    }
+    autoRunSlider();
+    
+    // When Hover fa ==============================================================
+    slider.find('.fa').on('mouseenter', function() { clearInterval(autoPlay); });
+    slider.find('.fa').on('mouseleave', function() { autoRunSlider(); });
 });
